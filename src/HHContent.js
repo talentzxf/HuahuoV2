1fd4a631056384ae1cf1759f257278a1e9e8a2ff
talentzxf <talentzxf@hotmail.com>
2022-02-14 12:12:59 +0800

    Continue learning HTML template and slots

--- src/HHContent.js
+++ src/HHContent.js
@@ -1,17 +1,65 @@
-export class HHContent extends HTMLElement {
+
+let howtoTabCounter = 0;
+
+class HHContent extends HTMLElement {
+    static get observedAttributes() {
+        return ['selected'];
+    }
+
     constructor() {
         super();
     }
 
-    connectedCallback(){
-        this.innerHTML = this.template
+    connectedCallback() {
+        this.setAttribute('role', 'tab');
+        if (!this.id)
+            this.id = `howto-tab-generated-${howtoTabCounter++}`;
+
+        this.setAttribute('aria-selected', 'false');
+        this.setAttribute('tabindex', -1);
+        this.style.height = 'fit-content'
+        this._upgradeProperty('selected');
     }
 
-    get template(){
-        return `
-        I'm the content now !!!
-        `
+    _upgradeProperty(prop) {
+        if (this.hasOwnProperty(prop)) {
+            let value = this[prop];
+            delete this[prop];
+            this[prop] = value;
+        }
+    }
+
+    attributeChangedCallback() {
+        const value = this.hasAttribute('selected');
+        this.setAttribute('aria-selected', value);
+        this.setAttribute('tabindex', value ? 0 : -1);
+    }
+
+    set selected(value) {
+        value = Boolean(value);
+        if (value)
+            this.setAttribute('selected', '');
+        else
+            this.removeAttribute('selected');
+    }
+
+    get selected() {
+        return this.hasAttribute('selected');
     }
 }
+customElements.define('hh-content', HHContent);
 
-window.customElements.define('hh-content', HHContent)
+let howtoPanelCounter = 0;
+
+class HowtoPanel extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.setAttribute('role', 'tabpanel');
+        if (!this.id)
+            this.id = `howto-panel-generated-${howtoPanelCounter++}`;
+    }
+}
+customElements.define('howto-panel', HowtoPanel);
--- src/HHPanel.js
+++ src/HHPanel.js
@@ -1,3 +1,5 @@
+import "./HHContent"
+
 const panelTemplateName = "HHPanel_Template"
 
 const KEYCODE = {
@@ -17,230 +19,173 @@ if (!template) {
       :host {
         display: flex;
         flex-wrap: wrap;
-      }
-      ::slotted(howto-panel) {
-        flex-basis: 100%;
+        height: 100%;
+        align-content: baseline;
       }
     </style>
-    <slot name="tab"></slot>
-    <slot name="panel"></slot>
+    <div name="tabs" style="flex-basis: 100%; height: fit-content">
+        <span style="border:1px solid gray">Tabs</span>
+    </div>
+    <div name="contents" style="flex-basis: 100%">
+        <span style="border:1px solid gray">Contents</span>
+    </div>
+    <slot></slot>
+<!--    <slot name="tab"></slot>-->
+<!--    <slot name="panel"></slot>-->
   `;
 }
 
-
-class HowtoTabs extends HTMLElement {
+class HHPanel extends HTMLElement {
     constructor() {
         super();
-        this._onSlotChange = this._onSlotChange.bind(this);
+        // this._onSlotChange = this._onSlotChange.bind(this);
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this._tabSlot = this.shadowRoot.querySelector('slot[name=tab]');
-        this._panelSlot = this.shadowRoot.querySelector('slot[name=panel]');
+        this._tabSlot = this.shadowRoot.querySelector('div[name=tabs]');
+        this._panelSlot = this.shadowRoot.querySelector('div[name=contents]');
 
-        this._tabSlot.addEventListener('slotchange', this._onSlotChange);
-        this._panelSlot.addEventListener('slotchange', this._onSlotChange);
+        // this._tabSlot.addEventListener('slotchange', this._onSlotChange);
+        // this._panelSlot.addEventListener('slotchange', this._onSlotChange);
     }
-
-    connectedCallback() {
-        this.addEventListener('keydown', this._onKeyDown);
-        this.addEventListener('click', this._onClick);
-
-        if (!this.hasAttribute('role'))
-            this.setAttribute('role', 'tablist');
-
-        Promise.all([
-            customElements.whenDefined('howto-tab'),
-            customElements.whenDefined('howto-panel'),
-        ]).then(_ => this._linkPanels());
-    }
-
-    disconnectedCallback() {
-        this.removeEventListener('keydown', this._onKeyDown);
-        this.removeEventListener('click', this._onClick);
-    }
-
-    _onSlotChange() {
-        this._linkPanels();
-    }
-
-    _linkPanels() {
-        const tabs = this._allTabs();
-        tabs.forEach(tab => {
-            const panel = tab.nextElementSibling;
-            if (panel.tagName.toLowerCase() !== 'howto-panel') {
-                console.error(`Tab #${tab.id} is not a` +
-                    `sibling of a <howto-panel>`);
-                return;
-            }
-
-            tab.setAttribute('aria-controls', panel.id);
-            panel.setAttribute('aria-labelledby', tab.id);
-        });
-
-        const selectedTab =
-            tabs.find(tab => tab.selected) || tabs[0];
-        this._selectTab(selectedTab);
-    }
-
-    _allPanels() {
-        return Array.from(this.querySelectorAll('howto-panel'));
-    }
-
-    _allTabs() {
-        return Array.from(this.querySelectorAll('howto-tab'));
-    }
-
-    _panelForTab(tab) {
-        const panelId = tab.getAttribute('aria-controls');
-        return this.querySelector(`#${panelId}`);
-    }
-
-    _prevTab() {
-        const tabs = this._allTabs();
-        let newIdx = tabs.findIndex(tab => tab.selected) - 1;
-
-        return tabs[(newIdx + tabs.length) % tabs.length];
-    }
-
-    _firstTab() {
-        const tabs = this._allTabs();
-        return tabs[0];
-    }
-
-    _lastTab() {
-        const tabs = this._allTabs();
-        return tabs[tabs.length - 1];
-    }
-
-    _nextTab() {
-        const tabs = this._allTabs();
-        let newIdx = tabs.findIndex(tab => tab.selected) + 1;
-        return tabs[newIdx % tabs.length];
-    }
-
-    reset() {
-        const tabs = this._allTabs();
-        const panels = this._allPanels();
-
-        tabs.forEach(tab => tab.selected = false);
-        panels.forEach(panel => panel.hidden = true);
-    }
-
-    _selectTab(newTab) {
-        this.reset();
-        const newPanel = this._panelForTab(newTab);
-
-        if (!newPanel)
-            throw new Error(`No panel with id ${newPanelId}`);
-        newTab.selected = true;
-        newPanel.hidden = false;
-        newTab.focus();
-    }
-
-    _onKeyDown(event) {
-        if (event.target.getAttribute('role') !== 'tab')
-            return;
-        if (event.altKey)
-            return;
-
-        let newTab;
-        switch (event.keyCode) {
-            case KEYCODE.LEFT:
-            case KEYCODE.UP:
-                newTab = this._prevTab();
-                break;
-
-            case KEYCODE.RIGHT:
-            case KEYCODE.DOWN:
-                newTab = this._nextTab();
-                break;
-
-            case KEYCODE.HOME:
-                newTab = this._firstTab();
-                break;
-
-            case KEYCODE.END:
-                newTab = this._lastTab();
-                break;
-
-            default:
-                return;
-        }
-
-        event.preventDefault();
-        this._selectTab(newTab);
-    }
-
-    _onClick(event) {
-        if (event.target.getAttribute('role') !== 'tab')
-            return;
-
-        this._selectTab(event.target);
-    }
+    //
+    // connectedCallback() {
+    //     this.addEventListener('keydown', this._onKeyDown);
+    //     this.addEventListener('click', this._onClick);
+    //
+    //     if (!this.hasAttribute('role'))
+    //         this.setAttribute('role', 'tablist');
+    //
+    //     Promise.all([
+    //         customElements.whenDefined('howto-tab'),
+    //         customElements.whenDefined('howto-panel'),
+    //     ]).then(_ => this._linkPanels());
+    // }
+    //
+    // disconnectedCallback() {
+    //     this.removeEventListener('keydown', this._onKeyDown);
+    //     this.removeEventListener('click', this._onClick);
+    // }
+    //
+    // _onSlotChange() {
+    //     this._linkPanels();
+    // }
+    //
+    // _linkPanels() {
+    //     const tabs = this._allTabs();
+    //     tabs.forEach(tab => {
+    //         const panel = tab.nextElementSibling;
+    //         if (panel.tagName.toLowerCase() !== 'howto-panel') {
+    //             console.error(`Tab #${tab.id} is not a` +
+    //                 `sibling of a <howto-panel>`);
+    //             return;
+    //         }
+    //
+    //         tab.setAttribute('aria-controls', panel.id);
+    //         panel.setAttribute('aria-labelledby', tab.id);
+    //     });
+    //
+    //     const selectedTab =
+    //         tabs.find(tab => tab.selected) || tabs[0];
+    //     this._selectTab(selectedTab);
+    // }
+    //
+    // _allPanels() {
+    //     return Array.from(this.querySelectorAll('howto-panel'));
+    // }
+    //
+    // _allTabs() {
+    //     return Array.from(this.querySelectorAll('howto-tab'));
+    // }
+    //
+    // _panelForTab(tab) {
+    //     const panelId = tab.getAttribute('aria-controls');
+    //     return this.querySelector(`#${panelId}`);
+    // }
+    //
+    // _prevTab() {
+    //     const tabs = this._allTabs();
+    //     let newIdx = tabs.findIndex(tab => tab.selected) - 1;
+    //
+    //     return tabs[(newIdx + tabs.length) % tabs.length];
+    // }
+    //
+    // _firstTab() {
+    //     const tabs = this._allTabs();
+    //     return tabs[0];
+    // }
+    //
+    // _lastTab() {
+    //     const tabs = this._allTabs();
+    //     return tabs[tabs.length - 1];
+    // }
+    //
+    // _nextTab() {
+    //     const tabs = this._allTabs();
+    //     let newIdx = tabs.findIndex(tab => tab.selected) + 1;
+    //     return tabs[newIdx % tabs.length];
+    // }
+    //
+    // reset() {
+    //     const tabs = this._allTabs();
+    //     const panels = this._allPanels();
+    //
+    //     tabs.forEach(tab => tab.selected = false);
+    //     panels.forEach(panel => panel.hidden = true);
+    // }
+    //
+    // _selectTab(newTab) {
+    //     this.reset();
+    //     const newPanel = this._panelForTab(newTab);
+    //
+    //     if (!newPanel)
+    //         throw new Error(`No panel with id ${newPanelId}`);
+    //     newTab.selected = true;
+    //     newPanel.hidden = false;
+    //     newTab.focus();
+    // }
+    //
+    // _onKeyDown(event) {
+    //     if (event.target.getAttribute('role') !== 'tab')
+    //         return;
+    //     if (event.altKey)
+    //         return;
+    //
+    //     let newTab;
+    //     switch (event.keyCode) {
+    //         case KEYCODE.LEFT:
+    //         case KEYCODE.UP:
+    //             newTab = this._prevTab();
+    //             break;
+    //
+    //         case KEYCODE.RIGHT:
+    //         case KEYCODE.DOWN:
+    //             newTab = this._nextTab();
+    //             break;
+    //
+    //         case KEYCODE.HOME:
+    //             newTab = this._firstTab();
+    //             break;
+    //
+    //         case KEYCODE.END:
+    //             newTab = this._lastTab();
+    //             break;
+    //
+    //         default:
+    //             return;
+    //     }
+    //
+    //     event.preventDefault();
+    //     this._selectTab(newTab);
+    // }
+    //
+    // _onClick(event) {
+    //     if (event.target.getAttribute('role') !== 'tab')
+    //         return;
+    //
+    //     this._selectTab(event.target);
+    // }
 }
-customElements.define('howto-tabs', HowtoTabs);
+customElements.define('hh-panel', HHPanel);
 
-let howtoTabCounter = 0;
-
-class HowtoTab extends HTMLElement {
-    static get observedAttributes() {
-        return ['selected'];
-    }
-
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        this.setAttribute('role', 'tab');
-        if (!this.id)
-            this.id = `howto-tab-generated-${howtoTabCounter++}`;
-
-        this.setAttribute('aria-selected', 'false');
-        this.setAttribute('tabindex', -1);
-        this._upgradeProperty('selected');
-    }
-
-    _upgradeProperty(prop) {
-        if (this.hasOwnProperty(prop)) {
-            let value = this[prop];
-            delete this[prop];
-            this[prop] = value;
-        }
-    }
-
-    attributeChangedCallback() {
-        const value = this.hasAttribute('selected');
-        this.setAttribute('aria-selected', value);
-        this.setAttribute('tabindex', value ? 0 : -1);
-    }
-
-    set selected(value) {
-        value = Boolean(value);
-        if (value)
-            this.setAttribute('selected', '');
-        else
-            this.removeAttribute('selected');
-    }
-
-    get selected() {
-        return this.hasAttribute('selected');
-    }
-}
-customElements.define('howto-tab', HowtoTab);
-
-let howtoPanelCounter = 0;
-
-class HowtoPanel extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        this.setAttribute('role', 'tabpanel');
-        if (!this.id)
-            this.id = `howto-panel-generated-${howtoPanelCounter++}`;
-    }
-}
-customElements.define('howto-panel', HowtoPanel);
+export {HHPanel}
