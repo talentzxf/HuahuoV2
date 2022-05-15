e96362d1faa1d1a4965e7f2fab02f896c05b8d65
Zhang <vincentzhang@ea.com>
2022-05-15 17:12:01 +0800

    Continue fixing link errors

--- HuaHuoEngine/src/Profiler/TimeHelper.cpp
+++ HuaHuoEngine/src/Profiler/TimeHelper.cpp
@@ -4,11 +4,59 @@
 
 #include "TimeHelper.h"
 #include "C/Baselib_Timer.h"
+#include "Cpp/Time.h"
+#include "Cpp/Stopwatch.h"
 
 #if !WEB_ENV
+
+#include <algorithm>
+#include <windef.h>
+#include <WinBase.h>
+
+#ifndef DONT_STRIP
+#define DONT_STRIP(address) UNUSED(address)
+#endif
+
+namespace WinApi
+{
+    static inline Baselib_Timer_TickToNanosecondConversionRatio Baselib_Timer_GetTicksToNanosecondsConversionRatio()
+    {
+        LARGE_INTEGER frequency = {0};
+        BOOL result = QueryPerformanceFrequency(&frequency);
+        BaselibAssert(result == TRUE);
+        return Baselib_Timer_TickToNanosecondConversionRatio { Baselib_NanosecondsPerSecond, static_cast<uint64_t>(frequency.QuadPart) };
+    }
+
+    static inline Baselib_Timer_Ticks Baselib_Timer_GetHighPrecisionTimerTicks()
+    {
+        LARGE_INTEGER start = {0};
+        BOOL result = QueryPerformanceCounter(&start);
+        BaselibAssert(result == TRUE);
+        return start.QuadPart;
+    }
+}
+
+Baselib_Timer_TickToNanosecondConversionRatio Baselib_Timer_GetTicksToNanosecondsConversionRatio()
+{
+    return WinApi::Baselib_Timer_GetTicksToNanosecondsConversionRatio();
+}
+
+
+Baselib_Timer_Ticks Baselib_Timer_GetHighPrecisionTimerTicks()
+{
+    return WinApi::Baselib_Timer_GetHighPrecisionTimerTicks();
+}
+
 namespace Common
 {
-    static double Baselib_Timer_GetTimeSinceStartupInSeconds();
+    static double Baselib_Timer_GetTimeSinceStartupInSeconds()
+    {
+        // DONT_STRIP(detail::commonStartupTimeInitializer);
+        // The reason for having this as a scoped static and not a file static is to avoid the ordering
+        // problems of file static initialization.
+        static const baselib::Stopwatch s_Stopwatch = baselib::Stopwatch::StartNew();
+        return std::chrono::duration<double>(s_Stopwatch.GetElapsedTime()).count();
+    }
 }
 
 double Baselib_Timer_GetTimeSinceStartupInSeconds(){
