cc2397d9432b6607e39f2a52db92fdf1ab849abd
Zhang <vincentzhang@ea.com>
2022-07-16 22:46:30 +0800

    Fixed save bug

--- HHIDE/src/index.js
+++ HHIDE/src/index.js
@@ -25,6 +25,7 @@ import {faEdit} from "@fortawesome/free-regular-svg-icons";
 import {saveAs} from 'file-saver';
 import {Logger} from "hhcommoncomponents";
 import {fileLoader} from "./SceneView/FileLoader";
+import {huahuoEngine} from "hhenginejs/src";
 
 
 library.add(faMinus)
@@ -41,10 +42,20 @@ library.add(faEdit)
 dom.watch();
 
 function save() {
-    let Uint8Array = Module.writeObjectStoreInMemoryFile()
-    let blob = new Blob([Uint8Array], {type: "application/octet-stream"})
-    saveAs(blob, "huahuo.data")
-    Logger.info("Good!!")
+    // Restore current scene view.
+
+    let mainSceneView = document.querySelector("#mainScene")
+    let oldStoreId = huahuoEngine.GetCurrentStoreId()
+
+    try{
+        huahuoEngine.GetDefaultObjectStoreManager().SetDefaultStoreByIndex(mainSceneView.storeId)
+        let Uint8Array = Module.writeObjectStoreInMemoryFile()
+        let blob = new Blob([Uint8Array], {type: "application/octet-stream"})
+        saveAs(blob, "huahuo.data")
+        Logger.info("Good!!")
+    }finally {
+        huahuoEngine.GetDefaultObjectStoreManager().SetDefaultStoreByIndex(oldStoreId)
+    }
 }
 
 function load(fName, e) {
