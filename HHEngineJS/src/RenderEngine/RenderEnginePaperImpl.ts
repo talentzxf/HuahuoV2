54ab2772e8a30ed0c77bbe56198a143f64a530f7
Zhang <vincentzhang@ea.com>
2022-07-23 22:24:58 +0800

    Resize still looks wrong if multiple views

--- HHEngineJS/src/RenderEngine/RenderEnginePaperImpl.ts
+++ HHEngineJS/src/RenderEngine/RenderEnginePaperImpl.ts
@@ -6,7 +6,7 @@ import {RenderEngine2D} from "./RenderEngine2D";
 let bgLayerName = "background"
 let contentLayerName = "content"
 
-class RenderEnginePaperJs implements RenderEngine2D{
+class RenderEnginePaperJs implements RenderEngine2D {
 
     // From canvas to project index
     private canvasPaperMap: Map<HTMLCanvasElement, number> = new Map()
@@ -14,32 +14,32 @@ class RenderEnginePaperJs implements RenderEngine2D{
     private canvasOriginalTranslate: Map<paper.View, paper.Point> = new Map()
 
     private isPlayer = false
-    private aspectRatio:number = 4/3  //  W:H = 4:3
+    private aspectRatio: number = 4 / 3  //  W:H = 4:3
 
-    getLayerByName(layerName){
+    getLayerByName(layerName) {
         let project = this.getProject()
-        let targetLayers = project.layers.filter((layer)=>{
+        let targetLayers = project.layers.filter((layer) => {
             return layer.name == layerName
         })
-        if(targetLayers.length != 1)
+        if (targetLayers.length != 1)
             return null
         return targetLayers[0]
     }
 
-    get bgLayer(){
+    get bgLayer() {
         return this.getLayerByName(bgLayerName)
     }
 
-    get contentLayer(){
+    get contentLayer() {
         return this.getLayerByName(contentLayerName)
     }
 
-    private getProject(){
+    private getProject() {
         return paper.project
     }
 
-    private activateBgLayer(){
-        if(!this.bgLayer){
+    private activateBgLayer() {
+        if (!this.bgLayer) {
             let contentLayer = this.getProject().activeLayer
             contentLayer.name = contentLayerName
             let bgLayer = new paper.Layer();
@@ -49,29 +49,29 @@ class RenderEnginePaperJs implements RenderEngine2D{
         this.bgLayer.activate()
     }
 
-    private restoreContentLayer(){
+    private restoreContentLayer() {
         this.contentLayer.activate()
     }
 
-    private createViewRectangle(x, y, w, h, fillColor){
+    private createViewRectangle(x, y, w, h, fillColor) {
         this.activateBgLayer()
         // Convert all points from view->project coordinate
-        let leftUp = view.viewToProject(new paper.Point(x,y))
+        let leftUp = view.viewToProject(new paper.Point(x, y))
         let rightDown = view.viewToProject(new paper.Point(x + w, y + h))
 
         return new paper.Path.Rectangle({
-            point:[leftUp.x, leftUp.y],
-            size:[rightDown.x - leftUp.x, rightDown.y - leftUp.y],
-            strokeColor:'white',
+            point: [leftUp.x, leftUp.y],
+            size: [rightDown.x - leftUp.x, rightDown.y - leftUp.y],
+            strokeColor: 'white',
             fillColor: fillColor,
             selected: false
         })
     }
 
-    private getContentWH(canvasWidth, canvasHeight){
+    private getContentWH(canvasWidth, canvasHeight) {
         let returnWidth = canvasWidth
         let returnHeight = canvasWidth / this.aspectRatio
-        if(returnHeight > canvasHeight){
+        if (returnHeight > canvasHeight) {
             returnHeight = canvasHeight
             returnWidth = canvasHeight * this.aspectRatio
         }
@@ -79,56 +79,55 @@ class RenderEnginePaperJs implements RenderEngine2D{
         return [returnWidth, returnHeight]
     }
 
-    public zoomIn(step = 1.05){
+    public zoomIn(step = 1.05) {
         view.zoom *= step;
         this.clearBackground()
 
         Logger.debug("Current view zoom:" + view.zoom)
     }
 
-    public zoomOut(step = 1.05){
+    public zoomOut(step = 1.05) {
         view.zoom /= step;
         this.clearBackground()
 
         Logger.debug("Current view zoom:" + view.zoom)
     }
 
-    public zoomReset(){
+    public zoomReset() {
         view.zoom = 1.0
         this.clearBackground()
     }
 
-    public clearBackground(){
+    public clearBackground() {
 
         let canvasWidth = view.element.width
         let canvasHeight = view.element.height
         // Logger.debug("Canvas width:" + canvasWidth + ",height:" + canvasHeight)
 
-        if(this.bgLayer){
+        if (this.bgLayer) {
             this.bgLayer.removeChildren()
         }
 
         let [contentWidth, contentHeight] = this.getContentWH(canvasWidth, canvasHeight)
-        let xOffset = (canvasWidth - contentWidth)/2
-        let yOffset = (canvasHeight - contentHeight)/2
+        let xOffset = (canvasWidth - contentWidth) / 2
+        let yOffset = (canvasHeight - contentHeight) / 2
         let contentRect = this.createViewRectangle(xOffset, yOffset, contentWidth, contentHeight, "white")
         contentRect.sendToBack()
 
-        if(!this.isPlayer)
-        {
-            let bgRect = this.createViewRectangle(0,0, canvasWidth, canvasHeight, new paper.Color("lightgray"))
+        if (!this.isPlayer) {
+            let bgRect = this.createViewRectangle(0, 0, canvasWidth, canvasHeight, new paper.Color("lightgray"))
             bgRect.sendToBack()
         }
 
         this.restoreContentLayer()
     }
 
-    public getWorldPosFromView(x:number, y:number):Vector2{
-        let worldPos = paper.view.viewToProject(new paper.Point(x,y))
+    public getWorldPosFromView(x: number, y: number): Vector2 {
+        let worldPos = paper.view.viewToProject(new paper.Point(x, y))
         return new Vector2(worldPos.x, worldPos.y)
     }
 
-    public init(canvas: HTMLCanvasElement, isPlayer: boolean = false){
+    public init(canvas: HTMLCanvasElement, isPlayer: boolean = false) {
         console.log("Initing paper!!!!")
 
         this.isPlayer = isPlayer
@@ -139,39 +138,48 @@ class RenderEnginePaperJs implements RenderEngine2D{
         this.clearBackground()
 
         this.canvasPaperMap.set(canvas, paper.project.index)
-        this.canvasOriginalSize.set(paper.view, [canvas.width, canvas.height])
+        if (canvas.width > 0 && canvas.height > 0) {
+            this.canvasOriginalSize.set(paper.view, [canvas.width, canvas.height])
+        }
+
         this.canvasOriginalTranslate.set(paper.view, new paper.Point(0.0, 0.0))
     }
 
-    resize(width: number, height: number){
-        let originalSize = this.canvasOriginalSize.get(paper.view)
-        let originalContentDim = this.getContentWH(originalSize[0], originalSize[1])
-        let originalX = (originalSize[0] - originalContentDim[0])/2
-        let originalY = (originalSize[1] - originalContentDim[1])/2
+    resize(width: number, height: number) {
+        if (!this.canvasOriginalSize.has(paper.view)) {
+            if (width > 0 && height > 0) {
+                this.canvasOriginalSize.set(paper.view, [width, height])
+            }
+        } else {
+            let originalSize = this.canvasOriginalSize.get(paper.view)
+            let originalContentDim = this.getContentWH(originalSize[0], originalSize[1])
+            let originalX = (originalSize[0] - originalContentDim[0]) / 2
+            let originalY = (originalSize[1] - originalContentDim[1]) / 2
 
-        let currentContentDim = this.getContentWH(width, height)
-        let currentX = (width - currentContentDim[0])/2
-        let currentY = (height - currentContentDim[1])/2
+            let currentContentDim = this.getContentWH(width, height)
+            let currentX = (width - currentContentDim[0]) / 2
+            let currentY = (height - currentContentDim[1]) / 2
 
-        let ratio = currentContentDim[0]/originalSize[0]
+            let ratio = currentContentDim[0] / originalSize[0]
 
-        let beforeScalingOriginPos = paper.view.projectToView(new paper.Point(0,0)) // Current origin in view coordinate
-        paper.view.scaling.x = ratio
-        paper.view.scaling.y = ratio
+            let beforeScalingOriginPos = paper.view.projectToView(new paper.Point(0, 0)) // Current origin in view coordinate
+            paper.view.scaling.x = ratio
+            paper.view.scaling.y = ratio
 
-        console.log("Ratio:" + ratio)
+            console.log("Ratio:" + ratio)
 
-        let projectOriginPos = paper.view.projectToView(new paper.Point(0,0)) // Current origin in view coordinate
+            let projectOriginPos = paper.view.projectToView(new paper.Point(0, 0)) // Current origin in view coordinate
 
-        // let offset = new paper.Point(projectOriginPos.x- beforeScalingOriginPos.x, projectOriginPos.y - beforeScalingOriginPos.y)
-        let offset = new paper.Point(-projectOriginPos.x/ratio, -projectOriginPos.y/ratio)
-        paper.view.translate(offset)
-        paper.view.translate(new paper.Point(currentX/ratio, currentY/ratio))
+            // let offset = new paper.Point(projectOriginPos.x- beforeScalingOriginPos.x, projectOriginPos.y - beforeScalingOriginPos.y)
+            let offset = new paper.Point(-projectOriginPos.x / ratio, -projectOriginPos.y / ratio)
+            paper.view.translate(offset)
+            paper.view.translate(new paper.Point(currentX / ratio, currentY / ratio))
+        }
     }
 
-    public setDefaultCanvas(canvas:HTMLCanvasElement){
-        if(this.canvasPaperMap.has(canvas)){
-            let view:any = paper.project.view
+    public setDefaultCanvas(canvas: HTMLCanvasElement) {
+        if (this.canvasPaperMap.has(canvas)) {
+            let view: any = paper.project.view
             let originalActiveCanvas = view.getElement()
 
             let projectIndex = this.canvasPaperMap.get(canvas)
