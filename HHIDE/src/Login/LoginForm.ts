2f0764fbd0df60c32b56f10ba7a227361c4ae028
talentzxf <talentzxf@hotmail.com>
2022-08-01 19:15:48 +0800

    Working on CSS of login form

--- HHIDE/src/Login/LoginForm.ts
+++ HHIDE/src/Login/LoginForm.ts
@@ -10,9 +10,38 @@ class LoginForm extends HTMLElement {
         this.style.left = "50%"
         let container = document.createElement("form")
         container.innerHTML = "" +
+            "<style>" +
+            "form{" +
+            "   border: 3px solid #f1f1f1" +
+            "}" +
+            "" +
+            "/* Full-width inputs */\n" +
+            "input[type=text], input[type=password] {" +
+            "  width: 100%;" +
+            "  padding: 12px 20px;" +
+            "  margin: 8px 0;" +
+            "  display: inline-block;" +
+            "  border: 1px solid #ccc;" +
+            "  box-sizing: border-box;" +
+            "}" +
+            "" +
+            "/* Set a style for all buttons */" +
+            "button {" +
+            "  background-color: #04AA6D;" +
+            "  color: white;" +
+            "  padding: 14px 20px;" +
+            "  margin: 8px 0;" +
+            "  border: none;" +
+            "  cursor: pointer;" +
+            "  width: 100%;" +
+            "}" +
+            "</style>" +
             "<div class=container>" +
             "   <label for='uname'><b>Username</b></label>" +
             "   <input type='text' placeholder='Enter Username' name='username'> " +
+            "   <label for='pwd'><b>Password</b></label>" +
+            "   <input type='password' placeholder='Enter Password' name='password'> " +
+            "   <button type='submit'>Login</button>" +
             "</div>"
 
         this.appendChild(container)
