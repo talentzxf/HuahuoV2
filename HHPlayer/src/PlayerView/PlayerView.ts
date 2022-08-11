fb0eebd7bec8e7c670f5651b106e8d8b9ee96eaf
talentzxf <talentzxf@hotmail.com>
2022-08-11 15:44:34 +0800

    Make player view full screen

--- HHPlayer/src/PlayerView/PlayerView.ts
+++ HHPlayer/src/PlayerView/PlayerView.ts
@@ -26,8 +26,8 @@ class PlayerView extends HTMLElement {
     createCanvasContainer() {
         this.canvasContainer = document.createElement("div")
         this.canvasContainer.id = "CanvasContainer"
-        // this.canvasContainer.style.width = "100%"
-        // this.canvasContainer.style.height = "100%"
+        this.canvasContainer.style.width = "100%"
+        this.canvasContainer.style.height = "100%"
         this.canvasContainer.style.padding = "0"
         this.canvasContainer.style.margin = "0"
         this.canvasContainer.style.backgroundColor = "gray"
@@ -47,6 +47,44 @@ class PlayerView extends HTMLElement {
         this.ctx = this.canvas.getContext("2d")
     }
 
+    OnResize() {
+        if(window.getComputedStyle(this.parentElement).display == "none")
+            return;
+
+        let containerWidth = this.canvasContainer.clientWidth
+        let containerHeight = this.canvasContainer.clientHeight
+        let margin = 15
+        let canvasWidth = containerWidth - margin
+        let canvasHeight = containerHeight - margin
+
+        renderEngine2D.resize(this.canvas, canvasWidth, canvasHeight)
+
+        // if(canvasHeight > containerHeight){
+        //     canvasHeight = containerHeight * margin
+        //     canvasWidth = canvasHeight * this.aspectRatio
+        // }
+
+        this.canvas.width = canvasWidth
+        this.canvas.height = canvasHeight
+        this.canvas.style.width = canvasWidth + "px"
+        this.canvas.style.height = canvasHeight + "px"
+        this.canvas.style.position = "relative"
+        this.canvas.style.left = (containerWidth - canvasWidth) / 2 + "px"
+        this.canvas.style.top = (containerHeight - canvasHeight) / 2 + "px"
+        this.Redraw()
+    }
+
+    Redraw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.fillStyle = "white"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+        let previousCanvas = renderEngine2D.setDefaultCanvas(this.canvas)
+        renderEngine2D.clearBackground()
+        if(previousCanvas)
+            renderEngine2D.setDefaultCanvas(previousCanvas)
+    }
+
     connectedCallback(){
         if(!this.inited){
             this.style.width = "100%"
@@ -55,6 +93,12 @@ class PlayerView extends HTMLElement {
             this.createCanvasContainer()
             this.createCanvas()
             renderEngine2D.init(this.canvas)
+
+            window.addEventListener("resize", this.OnResize.bind(this))
+
+            let resizeObserver = new ResizeObserver(this.OnResize.bind(this))
+            resizeObserver.observe(this.canvasContainer)
+
 
             this.animationPlayer = new Player();
 
