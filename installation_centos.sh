0d657069a41af0129534cb18c0121f6bb850d172
Zhang <vincentzhang@ea.com>
2022-08-13 14:13:53 +0800

    Added installation file

---
+++ installation_centos.sh
@@ -0,0 +1,8 @@
+yum groupinstall 'Development Tools'
+yum install vim git -y
+yum install httpd -y
+yum install tar -y
+yum install bzip2  -y
+yum install npm nodejs -y
+yum install wget -y
+wget https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-linux-x86_64.sh
