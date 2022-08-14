648bea6f5f9c0766f1eb41f6a369c24ff67c895e
Zhang <vincentzhang@ea.com>
2022-08-14 08:26:27 +0800

    Upgrade npm

--- installation_centos.sh
+++ installation_centos.sh
@@ -4,6 +4,7 @@ yum install httpd -y
 yum install tar -y
 yum install bzip2  -y
 yum install npm nodejs -y
+npm install -g npm@latest
 yum install wget -y
 wget https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-linux-x86_64.sh
 chmod +x ./cmake-3.24.0-linux-x86_64.sh
