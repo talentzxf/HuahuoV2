479e96d2936a4fe265d34027ceadc78fd5dd2e4f
talentzxf <talentzxf@hotmail.com>
2022-08-17 17:35:43 +0800

    Continue working on register form

--- HHIDE/src/Login/RegisterForm.ts
+++ HHIDE/src/Login/RegisterForm.ts
@@ -5,7 +5,20 @@ import {CustomElement} from "hhcommoncomponents"
 })
 class RegisterForm extends HTMLElement {
     connectedCallback(){
-        this.innerHTML = "Please register!!!"
+        this.innerHTML =
+            "   <form id='registerForm'>" +
+            "   <div style='display: flex; flex-direction: row-reverse'>" +
+            "       <div id='registerFormCloseBtn' >" +
+            "           <img class='far fa-circle-xmark'></img>" +
+            "       </div>" +
+            "   </div>" +
+            "       <h3>Register Here</h3>" +
+            "       <label for='uname'><b>Username</b></label>" +
+            "       <input type='text' placeholder='Enter Username' name='username'> " +
+            "       <label for='pwd'><b>Password</b></label>" +
+            "       <input type='password' placeholder='Enter Password' name='password'> " +
+            "       <button id='Create'>Create User</button>" +
+            "   </form>"
     }
 }
 
