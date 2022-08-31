cc2945b6c61352dbae874c50a89f57b33e1f3ef1
Zhang <vincentzhang@ea.com>
2022-08-31 21:39:58 +0800

    Not sure how to handle i18next

--- HHIDE/src/init.ts
+++ HHIDE/src/init.ts
@@ -1,13 +1,23 @@
 import {huahuoEngine} from "hhenginejs";
+import i18next from "i18next";
 
+function showMainDiv(){
+    let loadingElement = document.querySelector("#loading") as HTMLDivElement
+    loadingElement.style.display = "none"
+    let mainDiv = document.querySelector("#mainScene") as HTMLDivElement
+    mainDiv.style.display = "block"
+}
 function init(){
     huahuoEngine.ExecuteAfterInited(()=>{
-        let loadingElement = document.querySelector("#loading") as HTMLDivElement
-        loadingElement.style.display = "none"
-        let mainDiv = document.querySelector("#mainScene") as HTMLDivElement
-        mainDiv.style.display = "block"
-    })
+        if(i18next.isInitialized)
+            showMainDiv()
 
+        i18next.on("load", function(load){
+            if(load.hasOwnProperty("en")){
+               showMainDiv()
+            }
+        })
+    })
 }
 
 export {init}
