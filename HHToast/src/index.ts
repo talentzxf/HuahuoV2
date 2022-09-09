e04de9b819303383b7273846d898c416bb939c51
talentzxf <talentzxf@hotmail.com>
2022-09-09 10:54:33 +0800

    Begin to write my own toast lib

---
+++ HHToast/src/index.ts
@@ -0,0 +1,8 @@
+import {PolymerElement, html} from "@polymer/polymer";
+
+window.onload = function(){
+    const alert = document.createElement("div")
+    alert.className = "alert alert-info"
+    alert.innerHTML = "asdfasdfasdf"
+    document.body.appendChild(alert)
+}
---
+++ HHToast/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:"development",
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use:["style-loader","css-loader"]
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'src/index.ejs',
+            // inject: false
+        }),
+    ]
+};
