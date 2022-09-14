4574337ab453c38ff7f38bdfefa9d2feb552a3ce
talentzxf <talentzxf@hotmail.com>
2022-09-14 10:38:23 +0800

    Continue working on store setup form

--- HHIDE/src/Utilities/StoreInfoForm.ts
+++ HHIDE/src/Utilities/StoreInfoForm.ts
@@ -1,5 +1,5 @@
 import {CustomElement} from "hhcommoncomponents";
-import {SceneView} from "../SceneView/SceneView";
+import {renderEngine2D, Player, huahuoEngine} from "hhenginejs"
 import {CSSDefines} from "./CSSDefines";
 import {HHForm} from "./HHForm";
 
@@ -7,9 +7,13 @@ import {HHForm} from "./HHForm";
     selector: "hh-store-info"
 })
 class StoreInfoForm extends HTMLElement implements HHForm{
+    form: HTMLFormElement
+    formCloseBtn: HTMLElement
     previewSceneContainer: HTMLDivElement
-    previewSceneView: SceneView
     selector: string;
+
+    previewCanvas: HTMLCanvasElement
+    previewAnimationPlayer: Player
 
     onOKCallback: Function;
 
@@ -22,17 +26,45 @@ class StoreInfoForm extends HTMLElement implements HHForm{
         this.innerHTML += CSSDefines.formStyle
 
         // Add title.
-        this.innerHTML += "<h3>Store Info</h3>"
+        this.innerHTML += "<form>" +
+            "   <div style='display: flex; flex-direction: row-reverse'>" +
+            "       <div id='formCloseBtn' >" +
+            "           <img class='far fa-circle-xmark'>" +
+            "       </div>" +
+            "   </div>" +
+            "<h3>Store Info</h3>" +
+            "</form>"
+
+        this.form = this.querySelector("form")
+        this.formCloseBtn = this.querySelector("#formCloseBtn")
+        this.formCloseBtn.addEventListener("mousedown", this.closeForm.bind(this))
 
         this.previewSceneContainer = document.createElement("div")
-        this.previewSceneView = document.createElement("hh-sceneview") as SceneView
-        this.previewSceneContainer.appendChild(this.previewSceneView)
+        this.previewCanvas = document.createElement("canvas") as HTMLCanvasElement
+        this.previewCanvas.style.border = "1px solid blue"
+        this.previewSceneContainer.appendChild(this.previewCanvas)
+        this.form.appendChild(this.previewSceneContainer)
 
-        this.appendChild(this.previewSceneContainer)
+        this.previewAnimationPlayer = new Player()
+        renderEngine2D.init(this.previewCanvas)
+        this.RedrawFrame(0)
     }
 
     closeForm() {
         this.style.display = "none"
+    }
+
+    RedrawFrame(frameId:number){
+        let prevStore = huahuoEngine.GetCurrentStoreId()
+        huahuoEngine.GetDefaultObjectStoreManager().SetDefaultStoreByIndex(0) // Only render the main store.
+        let previousCanvas = renderEngine2D.setDefaultCanvas(this.previewCanvas)
+        renderEngine2D.clearBackground()
+        huahuoEngine.Set
+        this.previewAnimationPlayer.setFrameId(frameId)
+        if(previousCanvas)
+            renderEngine2D.setDefaultCanvas(previousCanvas)
+
+        huahuoEngine.GetDefaultObjectStoreManager().SetDefaultStoreByIndex(prevStore)
     }
 }
 
