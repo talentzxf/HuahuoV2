ae2b144ef9f75b9f719a0a1c7f1952a4c13f933f
talentzxf <talentzxf@hotmail.com>
2022-09-21 16:41:52 +0800

    Added loading for player

--- HHPlayer/src/index.js
+++ HHPlayer/src/index.js
@@ -1,12 +1,17 @@
 import {huahuoEngine, BaseShapeJS, shapeFactory} from "hhenginejs";
 import {PlayerView} from "./PlayerView/PlayerView";
 import {animationLoader} from "./PlayerView/AnimationLoader";
+import {init} from "./init";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 const projectId = urlParams.get("projectId")
 
-if(projectId)
-    animationLoader.loadAnimation(projectId)
-else
-    window.alert("Can't get projectId from URL")
+init()
+
+huahuoEngine.ExecuteAfterInited(()=>{
+    if(projectId)
+        animationLoader.loadAnimation(projectId)
+    else
+        window.alert("Can't get projectId from URL")
+})
---
+++ HHPlayer/src/init.ts
@@ -0,0 +1,21 @@
+import {huahuoEngine} from "hhenginejs";
+import {HHToast} from "hhcommoncomponents";
+
+function showMainDiv(){
+    let loadingElement = document.querySelector("#loading") as HTMLDivElement
+    loadingElement.style.display = "none"
+    let mainDiv = document.querySelector("#mainSceneDiv") as HTMLDivElement
+    mainDiv.style.display = "block"
+}
+
+let i18n = (window as any).i18n
+
+function init(){
+    huahuoEngine.ExecuteAfterInited(()=>{
+        i18n.ExecuteAfterInited(()=>{
+            showMainDiv()
+        })
+    })
+}
+
+export {init}
