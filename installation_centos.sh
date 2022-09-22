e7b30f54e31a62298f4559a061b8224d859c5e87
talentzxf <talentzxf@hotmail.com>
2022-09-22 10:39:25 +0800

    Begin to separate env for backend

--- installation_centos.sh
+++ installation_centos.sh
@@ -9,3 +9,7 @@ yum install wget -y
 wget https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-linux-x86_64.sh
 chmod +x ./cmake-3.24.0-linux-x86_64.sh
 source ./cmake-3.24.0-linux-x86_64.sh # Finish the cmake installation manually
+wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+chmod +x mariadb_repo_setup
+sudo ./mariadb_repo_setup
+sudo systemctl start mariadb.service
