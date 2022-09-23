e42ca4814944a41d2999d373d15c6d5c41a8274a
talentzxf <talentzxf@hotmail.com>
2022-09-23 08:03:19 +0800

    Changed player related config

--- installation_centos.sh
+++ installation_centos.sh
@@ -14,3 +14,25 @@ chmod +x mariadb_repo_setup
 sudo ./mariadb_repo_setup
 sudo yum install MariaDB-server MariaDB-client MariaDB-backup
 sudo systemctl start mariadb.service
+
+# cert
+
+# https://www.techrepublic.com/article/how-to-enable-https-on-apache-centos/
+
+sudo yum install mod_ssl openssl -y
+sudo openssl genrsa -out ca.key 2048
+sudo openssl req -new -key ca.key -out ca.csr
+sudo openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
+sudo cp ca.crt /etc/pki/tls/certs
+sudo cp ca.key /etc/pki/tls/private/ca.key
+sudo cp ca.csr /etc/pki/tls/private/ca.csr
+
+sudo systemctl restart httpd
+
+# Create virtual host
+sudo mkdir -p /var/www/html/adorkable
+sudo mkdir -p /etc/httpd/sites-available
+sudo mkdir -p /etc/httpd/sites-enabled
+
+# Create a
+sudo ln -s /etc/httpd/sites-available/adorkable.conf /etc/httpd/sites-enabled/adorkable.conf
