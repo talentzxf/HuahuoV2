6d819e7de4bd37fbceaf33cd95120ba712aa5b6e
Zhang <vincentzhang@ea.com>
2022-09-25 15:48:01 +0800

    Can show the shape and move the shape, but doesn't look right so far

--- HHEngineJS/src/Shapes/SVGShapeJS.ts
+++ HHEngineJS/src/Shapes/SVGShapeJS.ts
@@ -1,38 +1,54 @@
 import {BaseSolidShape} from "./BaseSolidShape";
 
 let shapeName = "BaseShape"
-class SVGShapeJS extends BaseSolidShape{
-    static createShape(rawObj){
+
+class SVGShapeJS extends BaseSolidShape {
+    static createShape(rawObj) {
         return new SVGShapeJS()
     }
 
     shapeURL: string
+
     getShapeName(): string {
         return shapeName;
     }
 
-    setShapeURL(shapeURL){
+    setShapeURL(shapeURL) {
         this.shapeURL = shapeURL
     }
 
-    createShape(){
+    createShape() {
         super.createShape()
 
         let _this = this
         let paperJs = this.getPaperJs()
-        paperJs["project"]["importSVG"](this.shapeURL, function(item){
+        paperJs["project"]["importSVG"](this.shapeURL, function (item) {
             _this.paperShape = item
             _this.paperShape.applyMatrix = false
             // _this.paperShape.strokeColor = new paper.Color("black")
             // _this.paperShape.fillColor = new paper.Color("green")
             _this.paperShape.data.meta = this
 
+            // Recurisively set the meta.
+            let shapeStack: Array<paper.Item> = new Array()
+            shapeStack.push(_this.paperShape)
+            while (shapeStack.length > 0) {
+                let currentShape = shapeStack.pop()
+                currentShape.data.meta = _this
+
+                if(currentShape.children){
+                    for (let child of currentShape.children) {
+                        shapeStack.push(child)
+                    }
+                }
+            }
+
             _this.afterCreateShape()
         })
     }
 
-    update(){
-        if(this.paperShape){
+    update() {
+        if (this.paperShape) {
             super.update()
         }
     }
