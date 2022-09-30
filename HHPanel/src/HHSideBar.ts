74fa42d0d20cbc71a4154f3a75e01813917ce2e2
Zhang <vincentzhang@ea.com>
2022-09-30 23:04:31 +0800

    Continue working on dockable sidebar

--- HHPanel/src/HHSideBar.ts
+++ HHPanel/src/HHSideBar.ts
@@ -1,36 +1,29 @@
 import {CustomElement} from "hhcommoncomponents";
+import {HHTitle} from "./HHTitle";
+import {HHContent} from "./HHContent";
 
 @CustomElement({
     selector: "hh-sidebar",
 })
 class HHSideBar extends HTMLElement{
-    titleBar:HTMLElement
+    titleBar:HHTitle
 
     isMoving: boolean = false
+    startMouseX: number = -1
+    startMouseY: number = -1
+    startPosX: number = -1
+    startPosY: number = -1
 
     connectedCallback(){
-        this.titleBar = document.createElement("div")
-        this.titleBar.onmousedown = this.onTitleMouseDown.bind(this)
-        this.titleBar.onmousemove = this.onTitleMouseMove.bind(this)
-        this.titleBar.onmouseup = this.onTitleMouseUp.bind(this)
+        let content = this.querySelector("hh-content") as HHContent
+        let title = content.getAttribute("title") || "No Title"
 
-        this.titleBar.innerText = this.getAttribute("title") || "No title"
-        this.style.position = "absolute"
-
+        this.titleBar = document.createElement("hh-title") as HHTitle
+        this.titleBar.appendChild(content)
+        this.titleBar.innerHTML = title
+        this.titleBar.setContent(content)
         this.insertBefore(this.titleBar, null)
         this.style.background = "lightgray"
-    }
-
-    onTitleMouseDown(evt: MouseEvent){
-        if(evt.)
-    }
-
-    onTitleMouseMove(evt: MouseEvent){
-
-    }
-
-    onTitleMouseUp(evt: MouseEvent){
-
     }
 }
 
