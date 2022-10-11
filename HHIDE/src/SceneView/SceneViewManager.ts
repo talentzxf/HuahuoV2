a4597b6df301d820fc51a92eb70f5156e9cd94ac
talentzxf <talentzxf@hotmail.com>
2022-10-11 08:21:43 +0800

    Save/Restore frameId for all the sceneview

--- HHIDE/src/SceneView/SceneViewManager.ts
+++ HHIDE/src/SceneView/SceneViewManager.ts
@@ -1,14 +1,23 @@
 import {SceneView} from "./SceneView";
 import {Logger} from "hhcommoncomponents"
 import {Player} from "hhenginejs"
+import {huahuoEngine} from "hhenginejs";
 
 class SceneViewManager{
     // Map from storeId->SceneView
     sceneViews: Map<number, SceneView> = new Map()
     curFocusedSceneView: SceneView = null
 
+    // Map from sceneview to frameId, when switch back to this scene, we need to restore it's frameId.
+    sceneViewFrameIdMap: Map<SceneView, number> = new Map()
+
     registerSceneView(sceneView: SceneView){
         this.sceneViews.set(sceneView.storeId, sceneView)
+    }
+
+    getSceneViewFrameId(sceneView: SceneView){
+        let storeId = sceneView.storeId
+        return huahuoEngine.GetStoreById(storeId).GetCurrentLayer().GetCurrentFrame()
     }
 
     getSceneView(storeId:number){
@@ -18,7 +27,26 @@ class SceneViewManager{
         return null
     }
 
+    setStoreFrameId(storeId, frameId){
+        let store = huahuoEngine.GetStoreById(storeId)
+        let layerCount = store.GetLayerCount()
+        for(let layerIdx = 0; layerIdx < layerCount; layerIdx++){
+            let targetLayer = store.GetLayer(layerIdx)
+            targetLayer.SetCurrentFrame(frameId)
+        }
+    }
+
     focusSceneView(sceneView:SceneView){
+        // save the currently focused scene view frameId.
+        if(this.curFocusedSceneView){
+            this.sceneViewFrameIdMap.set(this.curFocusedSceneView, this.getSceneViewFrameId(this.curFocusedSceneView))
+        }
+
+        // restore the saved frameId
+        if(this.sceneViewFrameIdMap.has(sceneView)){
+            this.setStoreFrameId(sceneView.storeId, this.sceneViewFrameIdMap.get(sceneView))
+        }
+
         this.curFocusedSceneView = sceneView
     }
 
