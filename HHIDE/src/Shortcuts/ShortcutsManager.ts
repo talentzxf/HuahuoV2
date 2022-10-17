240e11e8edab28ba51ce8c4ead93c870d528ae1d
talentzxf <talentzxf@hotmail.com>
2022-10-17 09:58:15 +0800

    Continue working on a sophisticated shortcut manager

--- HHIDE/src/Shortcuts/ShortcutsManager.ts
+++ HHIDE/src/Shortcuts/ShortcutsManager.ts
@@ -1,49 +1,141 @@
-enum ShortcutEvents{
+const SHORTKEYINTERVAL:number = 1 // Only within 1 seconds, count it as a valid series
+
+enum ShortcutEventNames{
     UNDO,
     REDO
 }
 
-class KeyCode{
-    ctrlKey: boolean
-    altKey: boolean
-    shiftKey: boolean
-    charCode: string
+function isAlphaNum(keyCode){
+    if(keyCode >= "0".charCodeAt(0) && keyCode <= "z".charCodeAt(0)){
+        return true
+    }
+    return false
+}
 
-    static fromKeyboardEvent(e:KeyboardEvent){
+class KeyCode{
+    ctrlKey ?: boolean = false
+    altKey ?: boolean = false
+    shiftKey ?: boolean = false
+    code: string
+
+    static fromConfig(obj){
         let retKeyCode = new KeyCode()
-        if(e.ctrlKey){
+        if(obj.ctrlKey){
             retKeyCode.ctrlKey = true
         }
 
-        if(e.altKey){
+        if(obj.altKey){
             retKeyCode.altKey = true
         }
 
-        if(e.shiftKey){
+        if(obj.shiftKey){
             retKeyCode.shiftKey = true
         }
 
-        if(e.key && e.key){
-
+        // Have to press a "solid" key to make this keycode valid
+        if(obj.key && isAlphaNum(obj.key)){
+            retKeyCode.code = obj.key
+        }else{
+            return null
         }
+
+        return retKeyCode
+    }
+
+    static fromKeyboardEvent(e:KeyboardEvent){
+        return this.fromConfig(e)
     }
 }
 
-class KeyCodeSeries{
-    keycodes:Array<KeyCode> = new Array<KeyCode>()
+// TODO: Do prefix search here.
+class KeyCodeSeriesTree{
+    children: Map<KeyCode, KeyCodeSeriesTree> = new Map<KeyCode, KeyCodeSeriesTree>()
+    action: Function // If it's a leaf, store it's action here
+
+    registerKeyCodeSeries(keyCodeConfigs:KeyCode[], action: Function){
+        if(keyCodeConfigs.length == 0){ // No more key code, this is leaf
+            this.action = action
+        }else{
+            let firstKeyCode = keyCodeConfigs[0]
+            let newChild = new KeyCodeSeriesTree()
+            newChild.registerKeyCodeSeries(keyCodeConfigs.slice(1), action)
+            this.children.set(firstKeyCode, newChild)
+        }
+    }
+
+    processKeyEvent(e: KeyCode): KeyCodeSeriesTree{
+        if(this.children.has(e)){
+            return this.children.get(e)
+        }
+        return null
+    }
 }
 
 class ShortcutsManager{
-    shortCuts: Array<KeyCodeSeries> = new Array<KeyCodeSeries>()
+    rootKeyCodeSeries: KeyCodeSeriesTree = new KeyCodeSeriesTree()
+    currentKeyCodeSeries: KeyCodeSeriesTree = null
+    lastPressedTime: number = -1
+
+    registeredShortCutHandlers: Map<ShortcutEventNames, Array<Function>> = new Map();
 
     init(){
         document.addEventListener("keypress", this.onKeyPressed.bind(this))
+
+        // Add default shortcuts
+        this.rootKeyCodeSeries.registerKeyCodeSeries([{
+            ctrlKey: true,
+            code:"z"
+        }], this.triggerEventFunc(ShortcutEventNames.UNDO))
+
+        // Add default shortcuts
+        this.rootKeyCodeSeries.registerKeyCodeSeries([{
+            ctrlKey: true,
+            altKey: true,
+            code:"z"
+        }], this.triggerEventFunc(ShortcutEventNames.REDO))
     }
 
-    onKeyPressed(){
+    registerShortcutHandler(eventName:ShortcutEventNames, callback: Function){
+        if(!this.registeredShortCutHandlers.has(eventName)){
+            this.registeredShortCutHandlers.set(eventName, new Array<Function>())
+        }
 
+        this.registeredShortCutHandlers.get(eventName).push(callback)
+    }
+
+    triggerEventFunc(eventName: ShortcutEventNames){
+        let _this = this
+        return function(){
+            for(let handler of _this.registeredShortCutHandlers.get(eventName)){
+                handler()
+            }
+        }
+    }
+
+    keyEventToKeyCode(e:KeyboardEvent): KeyCode{
+        return {
+            ctrlKey: e.ctrlKey,
+            shiftKey: e.shiftKey,
+            altKey: e.altKey,
+            code : e.key
+        }
+    }
+
+    onKeyPressed(e: KeyboardEvent){
+        if(this.currentKeyCodeSeries == null || (Date.now() - this.lastPressedTime) > SHORTKEYINTERVAL * 1000){
+            // Reset back to root
+            this.currentKeyCodeSeries = this.rootKeyCodeSeries
+        }
+
+        this.currentKeyCodeSeries = this.currentKeyCodeSeries.processKeyEvent(this.keyEventToKeyCode(e))
+        if(this.currentKeyCodeSeries.action != null){
+            this.currentKeyCodeSeries.action()
+            this.currentKeyCodeSeries = null
+        }
+
+        this.lastPressedTime = Date.now()
     }
 }
 
 let shortcutsManager = new ShortcutsManager()
-export {shortcutsManager}
+export {shortcutsManager, ShortcutEventNames}
