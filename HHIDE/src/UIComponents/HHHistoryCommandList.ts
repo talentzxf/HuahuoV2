5b67bad4942fe81dbf48114fd3e278f443929710
talentzxf <talentzxf@hotmail.com>
2022-10-19 15:46:22 +0800

    Make history panel resizable

--- HHIDE/src/UIComponents/HHHistoryCommandList.ts
+++ HHIDE/src/UIComponents/HHHistoryCommandList.ts
@@ -1,37 +1,52 @@
 import {CustomElement} from "hhcommoncomponents";
 import {undoManager} from "../RedoUndo/UndoManager";
+import {findParentPanel} from "hhpanel"
 
 @CustomElement({
     selector: "hh-history-commands"
 })
-class HHHistoryCommandList extends HTMLElement{
+class HHHistoryCommandList extends HTMLElement {
     commandListDiv: HTMLDivElement
-    connectedCallback(){
+    contentScrollerDiv: HTMLDivElement
+
+    connectedCallback() {
+        let parentPanel = findParentPanel(this)
+        let parentHeight = parentPanel ? parentPanel.clientHeight : 300;
+
+        this.contentScrollerDiv = document.createElement("div")
+        this.contentScrollerDiv.style.width = "100%"
+        this.contentScrollerDiv.style.height = parentHeight + "px"
+        this.contentScrollerDiv.style.overflowY = "auto"
+        this.contentScrollerDiv.style.overflowX = "auto"
+        this.contentScrollerDiv.style.resize = "both"
+        this.appendChild(this.contentScrollerDiv)
+
+
         this.commandListDiv = document.createElement("div")
-        this.commandListDiv.style.height = "100px"
-        this.commandListDiv.style.width = "500px"
+        this.commandListDiv.style.height = parentHeight + "px"
+        this.commandListDiv.style.width = "100%"
         this.commandListDiv.style.border = "1px solid blue"
         this.commandListDiv.style.overflow = "auto"
         this.commandListDiv.style.position = "relative"
-        this.appendChild(this.commandListDiv)
+        this.contentScrollerDiv.appendChild(this.commandListDiv)
 
         undoManager.registerListener(this.refreshCommands.bind(this))
         this.refreshCommands()
     }
 
-    refreshCommands(){
+    refreshCommands() {
         this.commandListDiv.innerHTML = ""
         let commands = undoManager.getCommands()
         let currentCommandIndex = undoManager.currentCmdIdx
 
         let currentSelecedCommandDiv: HTMLElement
 
-        for(let cmdIdx in commands){
+        for (let cmdIdx in commands) {
             let cmd = commands[cmdIdx]
             let commandDiv = document.createElement("div")
             let commandIdSpan = document.createElement("span")
 
-            let indicatorStr = (currentCommandIndex == parseInt(cmdIdx))?"->":""
+            let indicatorStr = (currentCommandIndex == parseInt(cmdIdx)) ? "->" : ""
 
             commandIdSpan.innerText = indicatorStr + cmdIdx.toString()
             commandIdSpan.style.paddingLeft = "30px"
@@ -44,16 +59,16 @@ class HHHistoryCommandList extends HTMLElement{
             commandDiv.appendChild(commandIdSpan)
             commandDiv.appendChild(commandContentSpan)
 
-            if(indicatorStr.length > 0){
+            if (indicatorStr.length > 0) {
                 currentSelecedCommandDiv = commandDiv
             }
 
             this.commandListDiv.prepend(commandDiv)
         }
 
-        if(currentSelecedCommandDiv != null){
+        if (currentSelecedCommandDiv != null) {
             let yDistanceToListDivTop = currentSelecedCommandDiv.offsetTop - this.commandListDiv.scrollTop
-            if(yDistanceToListDivTop > this.commandListDiv.clientHeight * 0.9 ||
+            if (yDistanceToListDivTop > this.commandListDiv.clientHeight * 0.9 ||
                 currentSelecedCommandDiv.offsetTop < this.commandListDiv.scrollTop + this.commandListDiv.clientHeight * 0.1)
                 this.commandListDiv.scroll(0, currentSelecedCommandDiv.offsetTop)
         }
