d18e1137897df287df32cc45784318918726f646
talentzxf <talentzxf@hotmail.com>
2022-11-10 17:53:33 +0800

    Saw the generated shapes, but still intervening the original shape

--- HHEngineJS/src/Components/GeneratorComponent.ts
+++ HHEngineJS/src/Components/GeneratorComponent.ts
@@ -1,6 +1,8 @@
 import {AbstractComponent, Component, PropertyValue} from "./AbstractComponent";
 import {PropertyCategory} from "./PropertySheetBuilder";
-import * as paper from "paper";
+import {BaseShapeJS} from "../Shapes/BaseShapeJS";
+import {FloatPropertyConfig} from "hhcommoncomponents";
+import {LoadShapeFromCppShape} from "../Shapes/LoadShape";
 
 //TODO: Move all these non default components into another sub-project
 @Component()
@@ -8,24 +10,38 @@ class GeneratorComponent extends AbstractComponent{
     @PropertyValue(PropertyCategory.shapeArray)
     targetShapeArray
 
-    paperShapeGroup: paper.Group
+    @PropertyValue(PropertyCategory.interpolateFloat, 0.1, {min:0.01, max:1.0, step: 0.01} as FloatPropertyConfig)
+    generateInterval
+
+    targetShapeGeneratedShapeArrayMap: Map<BaseShapeJS, Array<any>> = new Map<BaseShapeJS, Array<any>>()
+
     constructor(rawObj?) {
         super(rawObj);
+    }
 
-        this.paperShapeGroup = new paper.Group()
-        this.paperShapeGroup.applyMatrix = false
-        this.paperShapeGroup.data.meta = this.baseShape
+    afterUpdate() {
+        super.afterUpdate();
 
-        // let line1 = new paper.Path.Line(new paper.Point(0, 0), new paper.Point(1000, 0)) // X Axis
-        // let line2 = new paper.Path.Line(new paper.Point(0, 0), new paper.Point(0, 1000)) // Y Axis
-        //
-        // line1.strokeColor = new paper.Color("red")
-        // line1.strokeWidth = 1
-        // line2.strokeColor = new paper.Color("green")
-        // line2.strokeWidth = 1
-        //
-        // this.paperShapeGroup.addChild(line1)
-        // this.paperShapeGroup.addChild(line2)
+        for(let targetShape of this.targetShapeArray){
+            let baseShapeJS = this.baseShape.paperShape
+            let targetPaperShape = (targetShape as BaseShapeJS).paperShape
+            if(!this.targetShapeGeneratedShapeArrayMap.has(targetShape)){
+                let newGeneratedShapeArray = new Array<any>()
+                this.targetShapeGeneratedShapeArrayMap.set(targetShape, newGeneratedShapeArray)
+
+                // Duplicate shapes along the edge.
+                for(let currentLengthRatio = 0.0; currentLengthRatio < 1.0; currentLengthRatio += this.generateInterval){
+                    let currentLength = baseShapeJS.length * currentLengthRatio
+                    let rawObj = targetShape.rawObj
+                    let duplicatedShape = LoadShapeFromCppShape(rawObj)
+                    duplicatedShape.setSelectedMeta(null)
+
+                    let position = baseShapeJS.localToGlobal(baseShapeJS.getPointAt(currentLength))
+
+                    duplicatedShape.position = position
+                }
+            }
+        }
     }
 }
 
