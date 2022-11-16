c0372534ab98ab262b2f1fc62f82792be0e13c91
talentzxf <talentzxf@hotmail.com>
2022-11-16 14:54:22 +0800

    Gradually adding gradient color property

--- HHIDE/src/Inspector/InputComponents/HHColorStopArray.ts
+++ HHIDE/src/Inspector/InputComponents/HHColorStopArray.ts
@@ -1,4 +1,5 @@
 import {CustomElement} from "hhcommoncomponents";
+import {paper} from "hhenginejs"
 
 @CustomElement({
     selector: "hh-color-stop-array-input"
@@ -6,11 +7,22 @@ import {CustomElement} from "hhcommoncomponents";
 class HHColorStopArrayInput extends HTMLElement implements RefreshableComponent{
     getter: Function
     setter: Function
+
+    canvas: HTMLCanvasElement
     constructor(getter, setter) {
         super();
-        
+
         this.getter = getter
         this.setter = setter
+
+        this.canvas = document.createElement("canvas")
+
+        paper.setup(this.canvas)
+
+        let rectangle = new paper.Path.Rectangle(new paper.Point(0,0), new paper.Point(100,100))
+        rectangle.fillColor = new paper.Color("black")
+
+        this.appendChild(this.canvas)
     }
 
     refresh() {
