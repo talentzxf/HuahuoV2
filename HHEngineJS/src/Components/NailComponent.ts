c837c69e3dfb522f0049caaff3321a133511a302
Zhang <vincentzhang@ea.com>
2022-11-22 22:52:17 +0800

    Looks much better, but still a little bit strange

--- HHEngineJS/src/Components/NailComponent.ts
+++ HHEngineJS/src/Components/NailComponent.ts
@@ -2,16 +2,17 @@ import {AbstractComponent, Component, PropertyValue} from "./AbstractComponent";
 import {getNailManager} from "../IK/NailManager";
 import {Nail} from "../IK/Nail";
 
-const eps:number = 0.001;
+const eps: number = 0.001;
 
-@Component({compatibleShapes:["BaseSolidShape"], maxCount: 1})
-class NailComponent extends AbstractComponent{
+@Component({compatibleShapes: ["BaseSolidShape"], maxCount: 1})
+class NailComponent extends AbstractComponent {
     nails: Array<Nail> = new Array<Nail>()
 
     lastRotation: number = null
+    lastPosition: paper.Point = null
 
     // The coordinate of this hitPoint is in global world pos.
-    addNail(nail: Nail){
+    addNail(nail: Nail) {
         this.nails.push(nail)
     }
 
@@ -22,29 +23,35 @@ class NailComponent extends AbstractComponent{
         getNailManager().updateAllNails(currentFrame)
 
         // After update, the shape has returned to it's original rotation. Need to rotate back.
-        if(this.lastRotation){
+        if (this.lastRotation) {
             this.baseShape.paperShape.rotation = this.lastRotation
         }
 
+        if(this.lastPosition){
+            this.baseShape.paperShape.position = this.lastPosition
+        }
+
         // Adjust position and rotation to reflect the nail change.
-        for(let nail of this.nails){
+        for (let nail of this.nails) {
             let nailLocalPosition = nail.getNailLocalLocation(this.baseShape)
+
             let prevNailGlobalPosition = this.baseShape.localToGlobal(nailLocalPosition)
 
             let currentNailGlobalPosition = nail.position
 
-            if(prevNailGlobalPosition.getDistance(currentNailGlobalPosition) <= eps){
+            if (prevNailGlobalPosition.getDistance(currentNailGlobalPosition) <= eps) {
+                this.lastRotation = this.baseShape.paperShape.rotation
                 continue; // The position is not changed, no need to update my position
             }
 
-            let vector = currentNailGlobalPosition.subtract(prevNailGlobalPosition)
+            let vector = currentNailGlobalPosition.subtract(this.baseShape.position)
             this.baseShape.paperShape.rotation = vector.angle
 
-            console.log("Angle:" + vector.angle)
-            console.log("Directed Angle:" + prevNailGlobalPosition.getDirectedAngle(currentNailGlobalPosition))
-            this.baseShape.paperShape.position = this.baseShape.paperShape.position.add(vector)
+            let nailOffset = currentNailGlobalPosition.subtract(prevNailGlobalPosition)
+            this.baseShape.paperShape.position = this.baseShape.paperShape.position.add(nailOffset)
 
             this.lastRotation = this.baseShape.paperShape.rotation
+            this.lastPosition = this.baseShape.paperShape.position
         }
     }
 }
