9da348c49837814f5c0edfd4650b3fb410816261
Zhang <vincentzhang@ea.com>
2022-12-02 08:11:54 +0800

    Added taichi physical

---
+++ TaiChiPhysics/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:"development",
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hot Module Replacement',
+            template:"src/index.ejs"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
