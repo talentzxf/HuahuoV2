914ec3f0b913ec2f1579467276b086837672f648
talentzxf <talentzxf@hotmail.com>
2023-03-27 14:05:46 +0800

    Can rotate now, looks better

--- HHEngineJS/src/EventGraph/BaseShapeActions.ts
+++ HHEngineJS/src/EventGraph/BaseShapeActions.ts
@@ -19,7 +19,7 @@ class BaseShapeActions extends AbstractGraphAction{
     }
 
     execute(){
-        this.targetShape.rotateAroundPivot(this.rotation)
+        this.targetShape.rotateAroundPivot(this.rotation, false)
     }
 }
 
