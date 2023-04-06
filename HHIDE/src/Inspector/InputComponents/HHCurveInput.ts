1d8a8363ad69763b7f028f57864bfd5ce09a0e45
talentzxf <talentzxf@hotmail.com>
2023-04-06 11:40:11 +0800

    Set curve input background as lightgray

--- HHIDE/src/Inspector/InputComponents/HHCurveInput.ts
+++ HHIDE/src/Inspector/InputComponents/HHCurveInput.ts
@@ -3,12 +3,18 @@ import {CustomElement} from "hhcommoncomponents";
 @CustomElement({
     selector: "hh-curve-input"
 })
-class HHCurveInput extends HTMLElement{
+class HHCurveInput extends HTMLElement {
     canvas: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D
+
     constructor() {
         super();
 
         this.canvas = document.createElement("canvas")
+        this.ctx = this.canvas.getContext("2d")
+        this.ctx.globalCompositeOperation = "destination-over"
+        this.ctx.fillStyle = "lightgray"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 
         this.appendChild(this.canvas)
     }
