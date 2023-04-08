1992be1f6d9e660abd7ce1e8217401c1ba473887
VincentZhang <talentzxf@hotmail.com>
2023-04-08 22:13:10 +0800

    Coordinate is not correct

--- HHIDE/src/Inspector/InputComponents/HHCurveInput.ts
+++ HHIDE/src/Inspector/InputComponents/HHCurveInput.ts
@@ -1,4 +1,39 @@
 import {CustomElement} from "hhcommoncomponents";
+class ViewPort{
+    canvasWidth
+    canvasHeight
+
+    viewWidth
+    viewHeight
+
+    viewXMin
+    viewYMin
+    viewXMax
+    viewYMax
+    origin
+
+    get viewXSpan(){
+        if(this.viewXMax == this.viewXMin)
+            return 1
+        return this.viewXMax - this.viewXMin
+    }
+
+    get viewYSpan(){
+        if(this.viewYMin == this.viewYMax)
+            return 1;
+        return this.viewYMax - this.viewYMin
+    }
+
+    viewToCanvas(x, y){
+        let xScale = this.viewXSpan/this.viewWidth
+        let yScale = this.viewYSpan/this.viewHeight
+
+        let canvasX = this.origin[0] + x * xScale
+        let canvasY = this.canvasHeight - (this.origin[1] + y * yScale)
+
+        return [canvasX, canvasY]
+    }
+}
 
 @CustomElement({
     selector: "hh-curve-input"
@@ -8,6 +43,7 @@ class HHCurveInput extends HTMLElement {
     ctx: CanvasRenderingContext2D
 
     keyFrameCurveGetter: Function
+    viewPort: ViewPort = new ViewPort()
 
     constructor(keyFrameCurveGetter) {
         super();
@@ -32,38 +68,55 @@ class HHCurveInput extends HTMLElement {
             return
 
         let minValue = Number.MAX_VALUE
-        let maxValue = Number.MIN_VALUE
+        let maxValue = -Number.MAX_VALUE
 
         let minFrameId = Number.MAX_VALUE
-        let maxFrameId = Number.MIN_VALUE
+        let maxFrameId = -Number.MAX_VALUE
 
+        let points = []
         let totalPoints = curve.GetTotalPoints()
         for (let pointIdx = 0; pointIdx < totalPoints; pointIdx++) {
             let curvePoint = curve.GetKeyFrameCurvePoint(pointIdx)
             let value = curvePoint.GetValue()
-            let frameId = curvePoint.GetFrameId()
+            let frameId = curvePoint.GetFrameId() + 1 // In Cpp side, frameId starts from 0. But when shown, frameId starts from 1.
 
             minValue = Math.min(minValue, value)
             maxValue = Math.max(maxValue, value)
             minFrameId = Math.min(minFrameId, frameId)
             maxFrameId = Math.max(maxFrameId, frameId)
+
+            points.push(curvePoint)
         }
 
         // Draw the coordinate
-        let canvasWidth = this.canvas.width
-        let canvasHeight = this.canvas.height
+        this.viewPort.canvasWidth = this.canvas.width
+        this.viewPort.canvasHeight = this.canvas.height
+        this.viewPort.viewWidth = 0.9 * this.canvas.width
+        this.viewPort.viewHeight = 0.9 * this.canvas.height
+        this.viewPort.viewXMin = minFrameId
+        this.viewPort.viewXMax = maxFrameId
+        this.viewPort.viewYMin = minValue
+        this.viewPort.viewYMax = maxValue
+        this.viewPort.origin = [0.1 * this.canvas.width, 0.1 * this.canvas.height]
 
-        let origin = [0.1 * canvasWidth, 0.1 * canvasHeight]
-        let x = [0.9 * canvasWidth, 0.1 * canvasHeight]
-        let y = [0.1 * canvasWidth, 0.9 * canvasHeight]
+        let canvasOrigin = this.viewPort.viewToCanvas(0, 0)
+
+        let xMax = this.viewPort.viewToCanvas(maxFrameId,0)
+        let yMax = this.viewPort.viewToCanvas(0, maxValue)
+
         this.ctx.beginPath()
-        this.ctx.moveTo(origin[0], canvasHeight - origin[1])
-        this.ctx.lineTo(x[0], canvasHeight - x[1])
-        this.ctx.moveTo(origin[0], canvasHeight - origin[1])
-        this.ctx.lineTo(y[0], canvasHeight - y[1])
+        this.ctx.moveTo(canvasOrigin[0], canvasOrigin[1])
+        this.ctx.lineTo(xMax[0], xMax[1])
+        this.ctx.moveTo(canvasOrigin[0], canvasOrigin[1])
+        this.ctx.lineTo(yMax[0], yMax[1])
 
         this.ctx.strokeStyle = "green"
         this.ctx.stroke()
+
+        // Draw lines
+        for(let point of points){
+
+        }
     }
 
 }
