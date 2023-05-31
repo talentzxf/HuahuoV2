71717e3a6190cc1da8397e752ea79637306385ae
talentzxf <talentzxf@hotmail.com>
2023-05-31 15:13:50 +0800

    Follow curve looks good and code is much cleaner\!

--- HHEngineJS/src/Components/FollowCurveComponent.ts
+++ HHEngineJS/src/Components/FollowCurveComponent.ts
@@ -2,17 +2,21 @@ import {AbstractComponent, Component, PropertyValue} from "./AbstractComponent";
 import {PropertyCategory} from "./PropertySheetBuilder";
 import {ShapeArrayProperty} from "hhcommoncomponents";
 import {BaseShapeJS} from "../Shapes/BaseShapeJS";
+import {FloatPropertyConfig} from "hhcommoncomponents/dist/src/Properties/PropertyConfig";
+import {huahuoEngine} from "../EngineAPI";
 
 @Component({compatibleShapes: ["BaseShapeJS"], maxCount: 1})
 class FollowCurveComponent extends AbstractComponent {
     @PropertyValue(PropertyCategory.shape, null, {allowDuplication: false} as ShapeArrayProperty)
-    targetShape: BaseShapeJS
+    targetShape
 
     // From 0.0 - 100.0
     // 0.0 -- At the beginning
-    // 100.0 -- At the end.
-    @PropertyValue(PropertyCategory.interpolateFloat)
-    lengthPercentage: number = 0.0
+    // 1.0 -- At the end.
+    @PropertyValue(PropertyCategory.interpolateFloat, null, {max: 1.0, min: 0.0} as FloatPropertyConfig)
+    lengthRatio: number = 0.0
+
+    tsTargetShape: BaseShapeJS
 
     constructor(rawObj?) {
         super(rawObj);
@@ -21,8 +25,24 @@ class FollowCurveComponent extends AbstractComponent {
     }
 
     onTargetShapeChanged(followShape: BaseShapeJS){
-        let shapePosition = followShape.pivotPosition
+        this.tsTargetShape = followShape
+
+        let shapePosition = this.tsTargetShape.pivotPosition
         this.baseShape.getAction().setPosition(shapePosition.x, shapePosition.y)
+    }
+
+    override afterUpdate(force: boolean = false) {
+        super.afterUpdate(force);
+
+        if(this.baseShape.isVisible()){
+            if(this.tsTargetShape && this.tsTargetShape){
+                let totalLength = this.tsTargetShape.length()
+                let targetLength = totalLength * this.lengthRatio
+                let curvePoint = this.tsTargetShape.getPointAt(targetLength)
+
+                this.baseShape.getAction().setPosition(curvePoint.x, curvePoint.y)
+            }
+        }
     }
 
 }
