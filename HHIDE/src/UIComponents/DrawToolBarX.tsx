a0799fe1c3952152d9e560f4426d7c0834a34780
talentzxf <talentzxf@hotmail.com>
2023-09-16 08:35:15 +0800

    Added draw tool bar back

--- HHIDE/src/UIComponents/DrawToolBarX.tsx
+++ HHIDE/src/UIComponents/DrawToolBarX.tsx
@@ -1,9 +1,30 @@
 import * as React from "react"
+import {shapes} from "../ShapeDrawers/Shapes";
 
 class DrawToolBarX extends React.Component<any, any> {
     render() {
+        let toolBars = []
+        for (let shape of shapes) {
+            let imgSrc = null
+            let imgClass = null
+            if (shape.imgClass != "unknown_img") {
+                imgClass = shape.imgClass
+            } else {
+                imgSrc = shape.imgCss
+            }
+
+            toolBars.push((
+                <button>
+                    <img src={imgSrc} className={imgClass} title={i18n.t(shape.name)} style={{width: 20, height: 20}}
+                         onClick={shape.onClicked.bind(shape)}/>
+                </button>
+            ))
+        }
+
         return (<div>
-            ToolBar
+            {
+                toolBars
+            }
         </div>)
     }
 }
