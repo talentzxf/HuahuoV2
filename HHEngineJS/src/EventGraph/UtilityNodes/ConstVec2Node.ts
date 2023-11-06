112f65a213d0e650e4878a6bac69e16ac395ad9b
talentzxf <talentzxf@hotmail.com>
2023-11-06 18:35:14 +0800

    Fixed some bugs about ConstVec2Node

--- HHEngineJS/src/EventGraph/UtilityNodes/ConstVec2Node.ts
+++ HHEngineJS/src/EventGraph/UtilityNodes/ConstVec2Node.ts
@@ -1,5 +1,5 @@
 import {AbstractNode} from "../Nodes/AbstractNode";
-import {LiteGraph} from "litegraph.js";
+import {LiteGraph, SerializedLGraphNode} from "litegraph.js";
 
 class ConstVec2Node extends AbstractNode {
     title = "Const Vector2"
@@ -8,16 +8,40 @@ class ConstVec2Node extends AbstractNode {
     xWidget
     yWidget
 
+    properties = {
+        x: 1,
+        y: 1
+    }
+
     constructor() {
         super();
-        this.xWidget = this.addWidget("number", "valueX", 1, "valueX")
-        this.yWidget = this.addWidget("number", "valueY", 1, "valueY")
+        let _this = this
+        this.xWidget = this.addWidget("number", "valueX", this.properties.x, (v) => {
+            _this.properties.x = v
+            _this.graph.afterChange(_this)
+
+        })
+        this.yWidget = this.addWidget("number", "valueY", this.properties.y, (v) => {
+            _this.properties.y = v
+            _this.graph.afterChange(_this)
+        })
         this.addOutput("value", "vec2");
         this.widgets_up = true;
         this.size = [180, 60]
     }
 
+    onConfigure(o: SerializedLGraphNode) {
+        if (o.properties.hasOwnProperty("x"))
+            this.xWidget.value = o.properties.x
+
+        if (o.properties.hasOwnProperty("y"))
+            this.yWidget.value = o.properties.y
+    }
+
     onExecute() {
+        this.properties.x = this.xWidget.value
+        this.properties.y = this.yWidget.value
+
         this.setOutputData(0, {
             x: this.xWidget.value,
             y: this.yWidget.value
