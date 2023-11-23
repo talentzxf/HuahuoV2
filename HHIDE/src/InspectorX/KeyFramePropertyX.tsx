a1d9829074c3370dc73930e6cc90f8b1e5612f93
talentzxf <talentzxf@hotmail.com>
2023-11-23 18:02:08 +0800

    KeyFrame editorgit add .

--- HHIDE/src/InspectorX/KeyFramePropertyX.tsx
+++ HHIDE/src/InspectorX/KeyFramePropertyX.tsx
@@ -2,6 +2,12 @@ import * as React from "react"
 import {PropertyEntry, PropertyProps} from "./BasePropertyX";
 import {CSSUtils} from "../Utilities/CSSUtils";
 
+const canvasWidth = 200
+const canvasHeight = 50
+const penOffset = 10
+const textMargin = 5
+const spanRectHeight = 5
+
 type KeyFramePropertyState = {
     showDetails: boolean
 }
@@ -9,6 +15,10 @@ type KeyFramePropertyState = {
 class KeyFramePropertyX extends React.Component<PropertyProps, KeyFramePropertyState> {
     state: KeyFramePropertyState = {
         showDetails: false
+    }
+
+    constructor() {
+        super();
     }
 
     render() {
@@ -21,6 +31,12 @@ class KeyFramePropertyX extends React.Component<PropertyProps, KeyFramePropertyS
                 this.state.showDetails = !this.state.showDetails
                 this.setState(this.state)
             }}>{this.state.showDetails ? "^" : "v"}</button>
+            {
+                this.state.showDetails && <canvas width={canvasWidth} height={canvasHeight} style={{
+                    width: canvasWidth + "px",
+                    height: canvasHeight + "px"
+                }}></canvas>
+            }
         </PropertyEntry>
     }
 }
